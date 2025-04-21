a81ed35a2dc6108e935e501a07bb0a480c12c1e3
Daniel Dowd <daniel.h.dowd.mil@army.mil>
2025-04-21 13:54:36 -0500

    set up

---
+++ src/main/java/com/example/Inventory_Practice/InventoryPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Inventory_Practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Inventory_Practice/InventoryPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Inventory_Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
