a1049d4719737343fa56d3590080ca89e21bc2af
Daniel Dowd <daniel.h.dowd.mil@army.mil>
2025-04-23 16:05:28 -0500

    set up frontend with tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Form from './components/Form.tsx'
+
+function App() {
+
+  return (
+    <>
+        <h1>Inventory Form</h1>
+      <Form/>
+    </>
+  )
+}
+
+export default App
